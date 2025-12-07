3b20ac925b457a34d2956b28316ec11b7a804400
IA23005 <rubenedgardo70080503@gmail.com>
2025-12-06 19:58:17 -0600

    Cambios render 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-gestorq.onrender.com/api'
+  apiUrl: 'https://backend-gestorq.onrender.com/api',
+    auth: {
+    domain: 'dev-g4uoeyn0pe7d4spw.us.auth0.com',
+    clientId: 'FftIItHRId6QNnMeyE4ciLITontC6AM7'
+  }
 };
